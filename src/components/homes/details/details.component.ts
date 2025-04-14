554f037121a6d5157d7260b4efd04849f20b54e5
Jacob Menachem <dorlabor1@gmail.com>
2025-04-14 23:56:06 +0300

    feat: details forms

--- src/components/homes/details/details.component.ts
+++ src/components/homes/details/details.component.ts
@@ -2,10 +2,16 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../../../service/homes/housing.service';
 import { HousingLocation } from '../housing-location/housinglocation';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
 })
@@ -14,9 +20,24 @@ export class DetailsComponent {
   housingLocationId = -1;
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation =
       this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
+    this.applyForm.reset();
   }
 }
